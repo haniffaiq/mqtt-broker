bef4ec88c7aa358250d41730b670893e5907c925
hanif <haniffaiq95@gmail.com>
2025-09-10 15:02:10 +0700

    update

--- pubsub/basic_command.sh
+++ pubsub/basic_command.sh
@@ -1,19 +1,19 @@
-mosquitto_sub -h haniffaiq.com -p 1883 -t "test/topic"
+mosquitto_sub -h haniffaiq.com -p 1883 -t "public/topic"
 
-mosquitto_pub -h haniffaiq.com -p 1883 -t "test/topic" -m "Hello from Kali!"
+mosquitto_pub -h haniffaiq.com -p 1883 -t "public/topic" -m "Hello from Kali!"
 
-mosquitto_pub -h haniffaiq.com -p 1883 -t "test/topic" -m '{"device_id":"cli-01","temperature":30,"humidity":75,"status":"ok"}'
+mosquitto_pub -h haniffaiq.com -p 1883 -t "public/topic" -m '{"device_id":"cli-01","temperature":30,"humidity":75,"status":"ok"}'
 
 
 
 #WITH PASSWORD 
 
 
-mosquitto_sub -h haniffaiq.com -p 1883 -t "test/topic" -u hanif -P "Hanif123"
+mosquitto_sub -h haniffaiq.com -p 1883 -t "private/topic" -u hanif -P "Hanif123"
 
-mosquitto_pub -h haniffaiq.com -p 1883 -t "test/topic" -m "Hello from Kali!" -u hanif -P "Hanif123"
+mosquitto_pub -h haniffaiq.com -p 1883 -t "private/topic" -m "Hello from Kali!" -u hanif -P "Hanif123"
 
-mosquitto_pub -h haniffaiq.com -p 1883 -t "test/topic" -m '{"device_id":"cli-01","temperature":30,"humidity":75,"status":"ok"}' -u hanif -P "Hanif123"
+mosquitto_pub -h haniffaiq.com -p 1883 -t "private/topic" -m '{"device_id":"cli-01","temperature":30,"humidity":75,"status":"ok"}' -u hanif -P "Hanif123"
 
 
 
