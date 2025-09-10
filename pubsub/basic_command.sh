26ddcf3611d9c96d2da2ff4534233567a35a899a
hanif <haniffaiq95@gmail.com>
2025-09-10 14:56:33 +0700

    update

--- pubsub/basic_command.sh
+++ pubsub/basic_command.sh
@@ -6,4 +6,15 @@ mosquitto_pub -h haniffaiq.com -p 1883 -t "test/topic" -m '{"device_id":"cli-01"
 
 
 
+#WITH PASSWORD 
+
+
+mosquitto_sub -h haniffaiq.com -p 1883 -t "test/topic" -u hanif -P "Hanif123"
+
+mosquitto_pub -h haniffaiq.com -p 1883 -t "test/topic" -m "Hello from Kali!" -u hanif -P "Hanif123"
+
+mosquitto_pub -h haniffaiq.com -p 1883 -t "test/topic" -m '{"device_id":"cli-01","temperature":30,"humidity":75,"status":"ok"}' -u hanif -P "Hanif123"
+
+
+
 
